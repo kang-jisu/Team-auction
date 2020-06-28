a1c3bdb109605888e095c5f65ff191dc22436449
kang-jisu <rkdwltn0425@gmail.com>
2020-06-28 16:42:14 +0900

    테스트 코드 작성

---
+++ src/test/java/com/project/freelec/springboot/web/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.project.freelec.springboot.web;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    /*
+     * 스프링 MVC 테스트의 시작점
+     */
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+        mvc.perform(get("/hello")) // MockMvc를 통해 /hello주소로 http get요청 , 체이닝 지원
+                .andExpect(status().isOk()) // mvc.perform의 결과를 검증 . status가 200인지를 검증
+                .andExpect(content().string(hello)); // 응답 본문의 내용을 검증 ( "hello"와 맞는지)
+    }
+
+}
