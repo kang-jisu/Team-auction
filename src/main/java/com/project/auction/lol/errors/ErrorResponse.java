3a72859c25fe5a040eac707f09f9b1cd6b1acad2
kang-jisu <rkdwltn0425@gmail.com>
2021-08-28 03:25:20 +0900

    fix: handler에서 response값 반환 못하는 오류 - ErrorResponse @Getter 추가 #9

--- src/main/java/com/project/auction/lol/errors/ErrorResponse.java
+++ src/main/java/com/project/auction/lol/errors/ErrorResponse.java
@@ -1,10 +1,12 @@
 package com.project.auction.lol.errors;
 
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
 
+@Getter
 @ToString
 @NoArgsConstructor
 public class ErrorResponse {
